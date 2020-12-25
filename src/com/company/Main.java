7e6d60f3e7ebbdb2b6d86bf1baa55f115af40d5c
capybarin <vladibzd@gmail.com>
2020-12-25 14:52:25 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,28 +9,32 @@ import com.company.shapes.Triangle;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
-    public final static int MAX_NUMBER_OF_SHAPES = 20;
+    private final static int MAX_NUMBER_OF_SHAPES = 20;
 
     public static void main(String[] args) {
 
         List<Shape> shapes = new ArrayList<>();
         Color color;
 
+        int min = 1, max = 25;
+
         for (int i = 0; i < MAX_NUMBER_OF_SHAPES ; i++) {
+            int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
             color = Color.CYAN;
-            Square square = new Square(i+1, color);
+            Square square = new Square(randomNum, color);
 
             color = Color.MAGENTA;
-            Circle circle = new Circle(i+1, color);
+            Circle circle = new Circle(randomNum, color);
 
             color = Color.PINK;
-            Triangle triangle = new Triangle(i+3, i+4, i+5, color);
+            Triangle triangle = new Triangle(randomNum, randomNum, randomNum, color);
 
             color = Color.BLUE;
-            Trapezium trapezium = new Trapezium(i+5, i+2, i+3, color);
+            Trapezium trapezium = new Trapezium(randomNum, randomNum, randomNum, color);
 
             shapes.add(square);
             shapes.add(circle);
